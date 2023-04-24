f20f992f30ac969b0986bf181f959be2cc348f0e
akash-soni <akash.200287@gmail.com>
2023-04-24 18:59:25 +0530

    data ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,33 +1,68 @@
 import argparse
 import os
+import sys
 import shutil
 from tqdm import tqdm
 from src.logger import logging
-from src.utils.common import read_yaml, create_directories
+from src.exception import CustomException
+import pandas as pd
+from sklearn.model_selection import train_test_split
 import random
+from dataclasses import dataclass
 
 
-STAGE = "STAGE_NAME"  ## <<< change stage name
+STAGE = "DATA_INGESTION"  ## <<< change stage name
 
 
-def main(config_path, params_path):
-    ## read config files
-    config = read_yaml(config_path)
-    params = read_yaml(params_path)
-    pass
+@dataclass
+class DataIngestionConfig:
+    """
+    any inputs will be given using only config file
+    inside a class to define the class variable you __init__, but by using @decorator we can define
+    class variable directly.
+    """
+
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "data.csv")
+
+
+class DataIngestion:
+    def __init__(self) -> None:
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion methor or component")
+        try:
+            df = pd.read_csv("notebook\data\stud.csv")
+            logging.info("Read the datase as dataframe")
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True
+            )
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(
+                self.ingestion_config.train_data_path, index=False, header=True
+            )
+
+            test_set.to_csv(
+                self.ingestion_config.test_data_path, index=False, header=True
+            )
+
+            logging.info("Inmgestion of the data iss completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
 
 
 if __name__ == "__main__":
-    args = argparse.ArgumentParser()
-    args.add_argument("--config", "-c", default="configs/config.yaml")
-    args.add_argument("--params", "-p", default="params.yaml")
-    parsed_args = args.parse_args()
-
-    try:
-        logging.info("\n********************")
-        logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
-    except Exception as e:
-        logging.exception(e)
-        raise e
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()

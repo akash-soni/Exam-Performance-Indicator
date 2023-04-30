587c10dd338b26d64d9bcd90903e10e618a5b752
akash-soni <akash.200287@gmail.com>
2023-04-30 21:48:51 +0530

    hyperparameter tuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,15 +42,61 @@ class ModelTrainer:
                 test_array[:, -1],
             )
             models = {
-                "random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
-                "Gradient boost": GradientBoostingRegressor(),
+                "Random Forest": RandomForestRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "XGBRegressor": XGBRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor(),
                 "K-Neighbours Regression": KNeighborsRegressor(),
-                "XGBRegression": XGBRegressor(),
-                "CatBoosting Regression": CatBoostRegressor(verbose=False),
-                "AdaBoost Regression": AdaBoostRegressor(),
             }
+
+            params = {
+                "Decision Tree": {
+                    "criterion": [
+                        "squared_error",
+                        "friedman_mse",
+                        "absolute_error",
+                        "poisson",
+                    ],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest": {
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2',None],
+                    "n_estimators": [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "CatBoosting Regressor": {
+                    "depth": [6, 8, 10],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "iterations": [30, 50, 100],
+                },
+                "AdaBoost Regressor": {
+                    "learning_rate": [0.1, 0.01, 0.5, 0.001],
+                    # 'loss':['linear','square','exponential'],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "K-Neighbours Regression": {
+                    "n_neighbors": [5, 7, 9, 11, 13, 15],
+                    # "algorithm" :["ball_tree", "kd_tree", "brute"]
+                },
+            }
+
             # create model report based on the scores
             model_report: dict = evaluate_models(
                 X_train=X_train,
@@ -58,6 +104,7 @@ class ModelTrainer:
                 X_test=X_test,
                 y_test=y_test,
                 models=models,
+                param=params,
             )  # utils function which will evaluate all the models
 
             ## get best model score from dict
@@ -88,3 +135,8 @@ class ModelTrainer:
 
         except Exception as e:
             raise CustomException(e)
+
+
+if __name__ == "__main__":
+    modelTrainer = ModelTrainer()
+    print(modelTrainer.initiate_model_trainer(train_arr, test_arr))

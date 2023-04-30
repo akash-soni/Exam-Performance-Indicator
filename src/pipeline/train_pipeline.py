c261a02efa6b6e2bd9f02445994938d4df5bea01
akash-soni <akash.200287@gmail.com>
2023-04-30 16:46:37 +0530

    model training completed

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -10,6 +10,9 @@ from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
@@ -18,3 +21,6 @@ if __name__ == "__main__":
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
         train_data, test_data
     )
+
+    modelTrainer = ModelTrainer()
+    print(modelTrainer.initiate_model_trainer(train_arr, test_arr))
